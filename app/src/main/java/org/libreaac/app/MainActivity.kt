27318f823bb0f36a5dc7fdbd2e918385d4f5670f
Luke Howson <luke@libretastic>
2026-07-26 21:43:54 +1000

    Enable immersive kiosk display

--- app/src/main/java/org/libreaac/app/MainActivity.kt
+++ app/src/main/java/org/libreaac/app/MainActivity.kt
@@ -6,9 +6,12 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.util.Base64
+import android.view.View
+import android.view.WindowInsets
 import android.webkit.JavascriptInterface
 import android.webkit.MimeTypeMap
 import android.webkit.ValueCallback
@@ -35,12 +38,34 @@ class MainActivity : Activity(), TextToSpeech.OnInitListener {
         textToSpeech = TextToSpeech(this, this)
         webView = WebView(this)
         setContentView(webView)
+        enterImmersiveMode()
         configureWebView()
 
         if (savedInstanceState == null) {
             webView.loadUrl(APP_URL)
         } else {
             webView.restoreState(savedInstanceState)
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) enterImmersiveMode()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun enterImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.systemBars())
+        } else {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         }
     }
 
