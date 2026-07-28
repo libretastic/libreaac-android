eaa4092fcfd232739d194a47f59c1a750a1f80de
Luke Howson <luke@libretastic>
2026-07-28 10:07:56 +1000

    Release Android app 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "org.libreaac.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
