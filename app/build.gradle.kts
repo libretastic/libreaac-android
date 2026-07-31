20d75be5dc58935007eee97c425fc816d64ba020
Luke Howson <luke@libretastic>
2026-07-31 11:06:27 +1000

    Prepare Android release 0.1.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -149,8 +149,8 @@ android {
         applicationId = "org.libreaac.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.1.12"
+        versionCode = 14
+        versionName = "0.1.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
